ee8f46d61563a6663a4982d5a29c71b880b3a166
Mana-peiravian <man.peiravian@gmail.com>
2020-11-20 22:14:13 +0330

    implemented BattleSea methods

--- src/gameStuff/battleSea/BattleSea.java
+++ src/gameStuff/battleSea/BattleSea.java
@@ -7,7 +7,65 @@ public class BattleSea extends Game {
     public BattleSea(String gameID, String gameName) {
         super(gameID, gameName);
     }
+
     static public void startBattleSea(){
+
+    }
+
+    public void assignRandomCoordinates(Player player, Ship ship, int xPosition, int yPosition) {
+
+    }
+
+    public void changeShipCoordinates(Player player, Ship ship, int xPosition, int yPosition) {
+
+    }
+
+    public void setDirection(String direction){
+        //vertical or horizontal
+    }
+
+    public void changeCoordinateState(int xPosition, int yPosition){
+
+    }
+
+    public void boom(int xPosition, int yPosition){
+
+    }
+
+    public boolean isAvailableForAttack (int xPosition, int yPosition){
+        return false;
+    }
+
+    public boolean isAbleToAttack (Player player){
+        return false;
+        //if attack was successful, attack again
+    }
+
+    public void showShips(Player player){
+
+    }
+
+    public void showBooms(Player player){
+
+    }
+
+    public void showCorrectBooms(Player player){
+
+    }
+
+    public void showIncorrectBooms(Player player){
+
+    }
+
+    public  void showBoomedShips(Player player){
+
+    }
+
+    public void showUnboomedShips(Player player){
+
+    }
+
+    public void showCoordinatePlane(Player player){
 
     }
 }
