6dabeae223d322f75ab47188ecb90147022e6eea
Fatemeh <fatemeh.mir.99@gmail.com>
2020-12-14 00:36:03 +0330

    Phase 1; Dots And Boxes Controller.(1st)

--- src/plato/controller/DotsAndBoxesController.java
+++ src/plato/controller/DotsAndBoxesController.java
@@ -5,16 +5,32 @@ import plato.model.Player;
 
 
 public class DotsAndBoxesController extends DotsAndBoxes {
+    static final int WIDTH = 8;
+    static final int HEIGHT = 8;
+    static final int RIGHT = 0;
+    static final int BOTTOM = 1;
+    boolean[][] box;
+    int player1_score, player2_score;
+    int turn;
+    boolean[][] rightline;
+    boolean[][] bottomline;
 
     public DotsAndBoxesController(String gameID, String gameName) {
         super(gameID, gameName);
+        box = new boolean[WIDTH-1][HEIGHT-1];
+        rightline = new boolean[WIDTH-1][HEIGHT];
+        bottomline = new boolean[WIDTH][HEIGHT-1];
+        turn = 1;
     }
 
-    static public void startDotsAndBoxes(){
+    public static void startDotsAndBoxes(){
         executeGame();
     }
 
     public static void executeGame() {
+        while(true){
+
+        }
 
     }
     //Dots
@@ -26,10 +42,23 @@ public class DotsAndBoxesController extends DotsAndBoxes {
     }
     public void notChangeTurn(Player player){
 
+
     }
     //Line
-    public void drawLine(Player player){
-
+    public boolean drawLine(int width, int height, int direction){
+        // 0 for direction means right, 1 means bottom
+        if(width <0 || height<0) return false;
+        if(direction == RIGHT && width >= WIDTH-1) return false;
+        if(direction == BOTTOM && width >= WIDTH) return false;
+        if(direction == RIGHT && height >= HEIGHT) return false;
+        if(direction == BOTTOM && height >= HEIGHT-1) return false;
+        if(direction == BOTTOM && bottomline[width][height]) return false;
+        if(direction == RIGHT && rightline[width][height]) return false;
+        if(direction == BOTTOM)
+            bottomline[width][height] = true;
+        if(direction == RIGHT)
+            rightline[width][height] = true;
+        return true;
     }
 
     public void saveInformation(){
@@ -47,7 +76,8 @@ public class DotsAndBoxesController extends DotsAndBoxes {
     public void timeToDrawLine(){
 
     }
-    public void makeBox(Player player){
+    public void makeBox(){
+
 
     }
 
--- src/plato/view/GameView.java
+++ src/plato/view/GameView.java
@@ -5,16 +5,29 @@ import plato.model.GameLog;
 
 public class GameView {
 
-    public String showScoreboard(Game game) {
-        return null;
+    public void showScoreboard(Game game) {
+
     }
 
-    public String details(Game game) {
-        return null;
+    public void details(Game game) {
+        if (game.getGameName().equals("Dots And Boxes"))
+            System.out.println("Take more boxes than your opponent." +
+                    " You move by connecting two dots with a line." +
+                    " When you place the last 'wall' of a single square (box), the box is yours." +
+                    " The players move in turn, but whenever a player takes a box (s)he must move again.");
+        if (game.getGameName().equals("Battle Sea"))
+            System.out.println("Battle Sea is a strategy type guessing game for two players." +
+                    " It is played on ruled grids on which each player's fleet of ships  are marked." +
+                    " The locations of the fleets are concealed from the other player." +
+                    " Players alternate turns calling shots at the other player's ships," +
+                    " and the objective of the game is to destroy the opposing player's fleet.");
     }
 
-    public String showLog(GameLog game) {
-        return null;
+    public void showLog(GameLog game) {
+        System.out.println("Number Of Playing:" + game.getNumberOfPlaying());
+        System.out.println("Number Of Wins:" + game.getNumberOfWins());
+        System.out.println("Log ID:" + game.getLogID());
+        System.out.println("Game Score:" + game.getGameScore());
     }
 
     public String showWinsCount(Game game) {
