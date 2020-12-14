5938b213e0713804fc8ff16bbe204b42bffd03d5
Fatemeh <fatemeh.mir.99@gmail.com>
2020-12-15 00:47:21 +0330

    Phase 1; Dots And Boxes View.(1st)

--- src/plato/view/DotsAndBoxesView.java
+++ src/plato/view/DotsAndBoxesView.java
@@ -1,9 +1,12 @@
 package plato.view;
 
+import plato.controller.DotsAndBoxesController;
 import plato.model.Player;
 
 
 public class DotsAndBoxesView extends GameView {
+
+
 
     public void showWhoseTurn(Player player1, Player player2){
 
@@ -20,7 +23,17 @@ public class DotsAndBoxesView extends GameView {
     public void showDrawnLines(){
 
     }
-    public void showGameResult(){
+    public static void showGameResult(Player player1, Player player2){
+        System.out.println(player1 + "score is" + DotsAndBoxesController.player1_score);
+        System.out.println(player2 + "score is" + DotsAndBoxesController.player2_score);
+        if(DotsAndBoxesController.player1_score > DotsAndBoxesController.player2_score)
+            System.out.println(player1 + "won");
+        else if(DotsAndBoxesController.player2_score > DotsAndBoxesController.player1_score)
+            System.out.println(player2 + "won");
+        else
+            System.out.println("equal");
+    }
+    public static void showScore(){
 
     }
 }
